144db041458acb0ec95ca09e5d7e00cc0975e790
Anurag Singh <anuragsingh4932@gmail.com>
2026-05-10 12:41:24 +0530

    fix bug

--- client/src/app/profile/profile.ts
+++ client/src/app/profile/profile.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Router } from '@angular/router';
+import { RideService } from '../ride-service';
 
 @Component({
   selector: 'app-profile',
@@ -9,23 +10,29 @@ import { Router } from '@angular/router';
   styleUrl: './profile.css',
 })
 export class Profile {
-    user:any;
-  
-    router = inject(Router);
+    user: any;
+
+  rideDetails = signal({
+    totalRides: 0,
+    distanceTravelled: 0,
+    totalSpent: 0
+  });
+
+  rideService = inject(RideService);
+  router = inject(Router);
+
   ngOnInit() {
-  const userData = localStorage.getItem("user");
+    const userData = localStorage.getItem('user');
 
-  if (userData) {
-    this.user = JSON.parse(userData);
+    if (userData) {
+      this.user = JSON.parse(userData);
+
+      this.rideService.getProfile().subscribe((res: any) => {
+        console.log(res);
+        this.rideDetails.set(res);
+      });
+    }
   }
-}
 
 
-//navigation from dropdown menus
-  go_to_ride(){
-    this.router.navigate(['']);
-  }
-  mytrip(){
-    this.router.navigate(['my-trips'])
-  }
 }
