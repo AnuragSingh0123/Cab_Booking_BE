12a146b58f017b014345b2cdec9d48cccd4213df
Anurag Singh <Anurag.Singh286600@cognizant.com>
2026-05-22 11:09:45 +0530

    final

--- server/app.js
+++ server/app.js
@@ -10,7 +10,14 @@ const reviewRoutes = require("./routes/reviewRoutes");
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: `${process.env.FRONTEND_URL}`,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 
